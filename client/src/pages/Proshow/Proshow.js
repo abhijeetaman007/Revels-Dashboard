6e71969601c8d02b2a687d7a0afb2934e4b1fe89
RheaAdh <rheadhikari@gmail.com>
2022-03-12 19:48:07 +0530

    proshow card

--- client/src/pages/Proshow/Proshow.js
+++ client/src/pages/Proshow/Proshow.js
@@ -1,18 +1,82 @@
 import React from 'react';
 import Layout from '../Layout/Layout';
 import ArrowVector from '../../assets/right-arrow.png';
+import imgg from '../../assets/blue.png';
 const Proshow = () => {
   return (
     <Layout activeTab="proshow">
       <div className="proshow">
         <div className="card">
-          <button>
-            Buy Ticket
-            <img
-              style={{ height: '20px', marginLeft: '5px', marginTop: '3px' }}
-              src={ArrowVector}
-            ></img>
-          </button>
+          <img src={imgg}></img>
+          <center>
+            <button>
+              Buy Ticket
+              <img
+                style={{ height: '20px', marginLeft: '5px', marginTop: '3px' }}
+                src={ArrowVector}
+              ></img>
+            </button>
+          </center>
+        </div>
+        <div className="card">
+          <img src={imgg}></img>
+          <center>
+            <button>
+              Buy Ticket
+              <img
+                style={{ height: '20px', marginLeft: '5px', marginTop: '3px' }}
+                src={ArrowVector}
+              ></img>
+            </button>
+          </center>
+        </div>
+        <div className="card">
+          <img src={imgg}></img>
+          <center>
+            <button>
+              Buy Ticket
+              <img
+                style={{ height: '20px', marginLeft: '5px', marginTop: '3px' }}
+                src={ArrowVector}
+              ></img>
+            </button>
+          </center>
+        </div>
+        <div className="card">
+          <img src={imgg}></img>
+          <center>
+            <button>
+              Buy Ticket
+              <img
+                style={{ height: '20px', marginLeft: '5px', marginTop: '3px' }}
+                src={ArrowVector}
+              ></img>
+            </button>
+          </center>
+        </div>
+        <div className="card">
+          <img src={imgg}></img>
+          <center>
+            <button>
+              Buy Ticket
+              <img
+                style={{ height: '20px', marginLeft: '5px', marginTop: '3px' }}
+                src={ArrowVector}
+              ></img>
+            </button>
+          </center>
+        </div>
+        <div className="card">
+          <img src={imgg}></img>
+          <center>
+            <button>
+              Buy Ticket
+              <img
+                style={{ height: '20px', marginLeft: '5px', marginTop: '3px' }}
+                src={ArrowVector}
+              ></img>
+            </button>
+          </center>
         </div>
       </div>
     </Layout>
