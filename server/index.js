c5c05a4b2449a0f637e38da4cf9c453c816d6227
akash-4 <akashagarwal41200@gmail.com>
2022-03-17 22:36:27 +0530

    Major changes and fixes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,22 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://revelsmit.in", "http://localhost:3000"];
+  const origin = req.headers.origin;
+  console.log(origin);
+  if (allowedOrigins.indexOf(origin) > -1)
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  else if (process.env.NODE_ENV !== "production")
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "OPTIONS,GET,PUT,POST,DELETE, PATCH"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  return next();
+});
 app.use(express.json());
 app.use(
   express.urlencoded({
