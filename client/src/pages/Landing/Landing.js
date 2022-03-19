ec6ec8e095706029b2f41207b370c36465a4c0b8
PalChheda <chhedapal@yahoo.in>
2022-03-19 22:46:23 +0530

    Update Landing.js

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,18 +1,26 @@
 import React from "react";
 import "./Landing.scss"
+import Navbar from './Navbar/navbar.js'
 
 import tree from "./../../assets/backgrounds/tree.svg"; 
 import two from "./../../assets/backgrounds/two.svg";
 import sun from "./../../assets/backgrounds/sun.svg";
+import birds from "./../../assets/backgrounds/birds.svg";
 import three from "./../../assets/backgrounds/three.svg";
+import clouds_top from "./../../assets/backgrounds/clouds_top.svg";
+import four from "./../../assets/backgrounds/four.svg";
 
 const Landing  = () => {
   return (
     <div className="landing-wrapper">
       <div className="content-wrapper">
-        <div className="landing-nav"></div>
-        <div className="landing-title">
-          <h1 className="font-antiga">AAGAZ</h1>
+        <div className="landing-nav ">
+          <Navbar />
+        </div>
+        <div className="font-antiga landing-title">
+          <h5 className="subtitle1">CELEBRATING 40 YEARS OF REVELRY</h5>
+          <h1 className="title">AAGAZ</h1>
+          <h5 className="subtitle2">BEYOND LEGACIES</h5>
         </div>
       </div>
       <div className="svg-wrapper">
@@ -20,6 +28,9 @@ const Landing  = () => {
         <img src={two}></img>
         <img src={tree}></img>
         <img src={sun}></img>
+        <img src={birds}></img>
+        <img src={clouds_top}></img>
+        <img src={four}></img>
       </div>
     </div>
   );
