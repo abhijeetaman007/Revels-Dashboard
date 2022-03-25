304b51d181358eaca738b6362fc12ef22b95f4b7
Ishan Kumar <itsik159@gmail.com>
2022-03-25 13:22:36 +0530

    Lol

--- server/utils/file-upload/upload-controller.js
+++ server/utils/file-upload/upload-controller.js
@@ -3,12 +3,14 @@ const s3 = require('../../config/aws-s3/s3.config.js');
 // const {File} = require('../../models/file');
 // const {Task} = require('../../models/task');
 
-const doUpload = (key, file) => {
+const doUpload = async (key, file) => {
+  console.log("uplaod");
   const s3Client = s3.s3Client;
   const params = s3.uploadParams;
   params.Key = key;
   params.Body = file.buffer;
-  const s3upload = s3Client.upload(params).promise();
+  const s3upload = await s3Client.upload(params).promise();
+  console.log("uplaod last ",s3upload);
   return s3upload;
 };
 
