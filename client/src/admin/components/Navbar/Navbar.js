56a056d69a9f5a03fafa0015246c07dad1af69ea
RheaAdh <rheadhikari@gmail.com>
2022-03-26 06:03:05 +0530

    login admin

--- client/src/admin/components/Navbar/Navbar.js
+++ client/src/admin/components/Navbar/Navbar.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import revels from './../../../assets/logo_landing.png';
+import React from 'react';
+import revels from './../../../assets/logos/logo_landing.png';
 
 const Navbar = () => {
   return (
-    <div className='d-flex flex-row align-items-center'>
-        <img src={revels} height={100} className="mx-2 my-1"/>
-        <div className="font-light text-light" style={{fontSize:'1.5rem'}}>REVELS '22</div>
+    <div className="d-flex flex-row align-items-center">
+      <img src={revels} height={100} className="mx-2 my-1" />
+      <div className="font-light text-light" style={{ fontSize: '1.5rem' }}>
+        REVELS '22
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
