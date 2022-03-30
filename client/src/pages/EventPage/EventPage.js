3176fc0e853d3c5c95c69d3b2cc788e947179679
PalChheda <chhedapal@yahoo.in>
2022-03-31 00:39:53 +0530

    Update EventPage.js

--- client/src/pages/EventPage/EventPage.js
+++ client/src/pages/EventPage/EventPage.js
@@ -4,6 +4,8 @@ import EventCard from '../../components/EventCard/EventCard';
 import Lottie from 'lottie-react';
 import noEvents from '../../assets/noEvents.json';
 import Loader from '../Loader/Loader';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import './EventPage.css';
 
 const Events = () => {
   const [events, setEvents] = useState([]);
@@ -21,28 +23,68 @@ const Events = () => {
   }, []);
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: "center",
-        flexWrap: 'wrap',
-        gap: '10px',
-        height: 'fit-content',
-      }}
-    >
-      {events
-        ? events.map((eventData, index) => {
-            return <EventCard key={index} index={index} data={eventData} />;
-          })
-        : 
-        <Loader />
-      }
-      {events.length === 0 && 
-      <div className="py-5 w-md-100 w-50 mx-auto text-center d-flex flex-column justify-content-center align-items-center">
-          <Lottie animationData={noEvents} loop/>
-          <h3 className="font-heavy" style={{ color: "#c4515c", fontSize: "2rem" }}>NO EVENTS FOUND!</h3>
-        </div>}
-    </div>
+   <div className='page-wrapper'>
+        <Tabs className='w-100'>
+        <TabList className='w-100 d-flex justify-content-center align-items-center my-4'>
+        <Tab className="btn m-2 font-medium">
+              Sports
+          </Tab>
+          <Tab className="btn m-2 font-medium">
+              Cultural
+          </Tab>
+        </TabList>
+        <TabPanel>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: "center",
+            flexWrap: 'wrap',
+            gap: '10px',
+            height: 'fit-content',
+          }}
+        >
+          {events
+            ? events.map((eventData, index) => {
+              if(eventData.eventType==='SPORTS')
+                return <EventCard key={index} index={index} data={eventData} />;
+              })
+            : 
+            <Loader />
+          }
+          {events.length === 0 && 
+            <div className="py-5 w-md-100 w-50 mx-auto text-center d-flex flex-column justify-content-center align-items-center">
+                <Lottie animationData={noEvents} loop/>
+                <h3 className="font-heavy" style={{ color: "#c4515c", fontSize: "2rem" }}>NO EVENTS FOUND!</h3>
+            </div>}
+          </div>
+        </TabPanel>
+        <TabPanel>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: "center",
+            flexWrap: 'wrap',
+            gap: '10px',
+            height: 'fit-content',
+          }}
+        >
+          {events
+            ? events.map((eventData, index) => {
+              if(eventData.eventType==='CULTURAL')
+                return <EventCard key={index} index={index} data={eventData} />;
+              })
+            : 
+            <Loader />
+          }
+          {events.length === 0 && 
+            <div className="py-5 w-md-100 w-50 mx-auto text-center d-flex flex-column justify-content-center align-items-center">
+                <Lottie animationData={noEvents} loop/>
+                <h3 className="font-heavy" style={{ color: "#c4515c", fontSize: "2rem" }}>NO EVENTS FOUND!</h3>
+            </div>}
+          </div>
+        </TabPanel>
+      </Tabs>
+     </div>
   );
 };
 
