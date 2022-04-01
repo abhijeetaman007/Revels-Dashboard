7d8758c3f755c0a765a2ee265d048227c42a88ec
RheaAdh <rheadhikari@gmail.com>
2022-04-02 00:49:49 +0530

    join team bug fix event._id and status:verified in register user

--- client/src/components/EventCard/EventCard.js
+++ client/src/components/EventCard/EventCard.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import './EventCard.scss';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 const EventCard = ({ data, index }) => {
   const [eventPath, setEventPath] = React.useState(
     `/dashboard/event/${data._id}`
   );
-  const eventCardColours = ["event-back-1", "event-back-2"];
+  const eventCardColours = ['event-back-1', 'event-back-2'];
   return (
-    <div className={`event-card-wrapper card-up ${eventCardColours[index % 2]}`}>
+    <div
+      className={`event-card-wrapper card-up ${eventCardColours[index % 2]}`}
+    >
       <div className="event-content">
         <div className="event-header">
           <div className="event-area">
-            <p className="font-heavy">{data.category.category.toUpperCase()}</p>
+            <p className="font-heavy">{data.category.category}</p>
             <h3 className="font-light">{data.name}</h3>
           </div>
           <div className="button-area">
             <button className="font-heavy">
               {data.mode === 'OFFLINE' ? 'Offline' : 'Online'} Event
             </button>
-            {/* <Link to={eventPath} className="font-medium">
-              <button className="font-medium">Details</button>
-            </Link> */}
           </div>
+          <Link to={eventPath} className="button-area">
+            <button className="font-heavy">Details</button>
+          </Link>
         </div>
         <div className="tags-line">
           {data.tags != undefined
@@ -50,18 +52,22 @@ const EventCard = ({ data, index }) => {
               <h3 className="font-light">1</h3>
             </div>
           )}
-          {data.eventDateTime && <div className="box">
-            <p className="font-heavy">DATE</p>
-            <h3 className="font-light">
-              {new Date(data.eventDateTime).getDate()}/
-              {new Date(data.eventDateTime).getMonth()}/
-              {new Date(data.eventDateTime).getFullYear()}
-            </h3>
-          </div>}
-          {data.eventVenue && <div className="box">
-            <p className="font-heavy">VENUE</p>
-            <h3 className="font-light">{data.eventVenue}</h3>
-          </div>}
+          {data.eventDateTime && (
+            <div className="box">
+              <p className="font-heavy">DATE</p>
+              <h3 className="font-light">
+                {new Date(data.eventDateTime).getDate()}/
+                {new Date(data.eventDateTime).getMonth()}/
+                {new Date(data.eventDateTime).getFullYear()}
+              </h3>
+            </div>
+          )}
+          {data.eventVenue && (
+            <div className="box">
+              <p className="font-heavy">VENUE</p>
+              <h3 className="font-light">{data.eventVenue}</h3>
+            </div>
+          )}
         </div>
       </div>
     </div>
