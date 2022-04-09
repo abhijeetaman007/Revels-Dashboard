ad804ee7f96765bf752a9d2c9145858de3eefede
Abhijeet Sinha <abhijeetaman007@gmail.com>
2022-04-09 16:34:32 +0530

    vigilance event route

--- server/routes/admins/vigilance.js
+++ server/routes/admins/vigilance.js
@@ -24,11 +24,11 @@ const getUserFromID = async (req, res) => {
 const isEventRegistered = async (req, res) => {
   try {
     let { token, eventID } = req.params;
-    let user = await User.findOne({ token }, { _id: 1 });
+    let event = await Event.findOne({ eventID }, { _id: 1 , delegateCards:1});
+    let user = await User.findOne({ token , delegateCards:  {$all:event.delegateCards}}, { _id: 1 });
     console.log("User ", user);
     if (!user)
-      return res.status(400).send({ success: false, msg: "No user Found" });
-    let event = await Event.findOne({ eventID }, { _id: 1 });
+      return res.status(400).send({ success: false, msg: "No user Found Or delegate not purchased" });
     if (!event)
       return res.status(400).send({ success: false, msg: "No Event Found" });
     console.log("Event ", event);
