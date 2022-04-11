81cd23369f18e4156c3a1b72d4058eab7f7c28f1
RheaAdh <rheadhikari@gmail.com>
2022-04-12 00:43:08 +0530

    data

---
+++ client/src/pages/DevelopersPage/Developers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+const Developers = () => {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default Developers;
