e7e1272b8b3364e628f40d28e6681b5ed0e8fffd
Parthiv-M <parthiv.menon@learner.manipal.edu>
2022-04-13 00:36:52 +0530

    updated schedule

--- client/src/pages/Rulebook/PdfPage.js
+++ client/src/pages/Rulebook/PdfPage.js
@@ -11,7 +11,7 @@ const PdfPage = ({pdf}) => {
     } else if(pdf === "mitpost") {
         pdfPath = "/mitpost.pdf";
     } else if(pdf === "schedule-cultural") {
-        pdfPath = "/schedule0.pdf";
+        pdfPath = "/schedule1.pdf";
     } else if(pdf === "schedule-sports") {
         pdfPath = "/sportsschedule.pdf";
     }
